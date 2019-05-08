a4d5db6346add890addd1f69b9288cf967469955
Braden Walker <bradencodes@gmail.com>
2019-05-08 16:01:39 -0700

    add user routes

---
+++ server/routes/api/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Load input validation
+const validateRegisterInput = require("../../validation/register.js");
+const validateLoginInput = require('../../validation/login.js');
+
+// Load User model
+const User = require('../../models/User.js');
+
+// @route POST api/users/register
+// @desc Register new user
+// @access Public
+router.post("/register", (req, res) => {
+
+    // Form validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email already exists" });
+        }
+
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        // Hash password before storing
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+            })
+        })
+    })
+})
